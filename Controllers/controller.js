67e689e31e158754f442a4ec2fe5dc7f89099622
Sachinmehar21 <130161660+Sachinmehar21@users.noreply.github.com>
2024-09-22 02:52:21 +0530

    Error Handling/-
    Synchronous as well as Asynchronous Error handling.

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -1,3 +1,5 @@
+const { catchAsyncErrors } = require("../Middlewares/catchAsyncError");
+
 exports.homepage =  catchAsyncErrors(async (req,res,next) => {
     res.json({ message: 'Hello' });
 });
--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const app = express();
 const logger = require("morgan");
 app.use(logger("tiny"));
 
-//routes
+//Error handlinge
+const ErrorHandler = require("./Utils/Errorhandler");
+const { generatedErrors } = require("./Middlewares/Errors");
+app.get("*", (req, res, next) => {
+  next(new ErrorHandler(`Requested URL not Found ${req.url}`, 404));
+});
+app.use(generatedErrors);
+
 app.get("/", require("./routes/routes"));
 
 app.listen(
